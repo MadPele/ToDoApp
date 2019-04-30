62fe47403da7d600a61c28d611fac39df8d39dc5
Andrzej Derela <andrzejderela@gmail.com>
2019-04-30 10:40:32 +0200

    Add css file

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="todo-app">
       <Header />
       <TodoList />
       <Footer />
--- todoapp/src/components/Header.js
+++ todoapp/src/components/Header.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  var today = new Date();
+  today = today.toDateString();
+  return (
+    <div className="todo-header">
+      <h2>
+        What's your plan for today?
+        <br />
+        <span className="header-date">({today})</span>
+      </h2>
+    </div>
+  );
 }
 
 export default Header;
